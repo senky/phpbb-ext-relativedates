1c0628dfc3db94b4ab69b589c02caf7127a40d94
Jakub Senko <jakubsenko@gmail.com>
2017-04-06 21:15:41 +0200

    Update Russian (ru) translation

--- language/ru/common.php
+++ language/ru/common.php
@@ -20,64 +20,66 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'AGO'		=> '%s тому назад',
-	'FROM_NOW'	=> '%s с этого момента',
-	'AFTER'		=> '%s после',
-	'BEFORE'	=> '%s до',
-	'YEAR'		=> array(
+	'RELATIVE_DATES'	=> 'Показывать относительные даты',
+
+	'R_AGO'			=> '%s тому назад',
+	'R_FROM_NOW'	=> '%s отныне',
+	'R_AFTER'		=> '%s спустя',
+	'R_BEFORE'		=> '%s прежде чем',
+	'R_YEAR'		=> array(
 		1	=> '1 год',
 		2	=> '%d года',
 		3	=> '%d лет',
 	),
-	'MONTH'		=> array(
+	'R_MONTH'		=> array(
 		1	=> '1 месяц',
 		2	=> '%d месяца',
 		3	=> '%d месяцев',
 	),
-	'WEEK'		=> array(
+	'R_WEEK'		=> array(
 		1	=> '1 неделя',
 		2	=> '%d недели',
 		3	=> '%d недель',
 	),
-	'DAY'		=> array(
+	'R_DAY'		=> array(
 		1	=> '1 день',
 		2	=> '%d дня',
 		3	=> '%d дней',
 	),
-	'HOUR'		=> array(
+	'R_HOUR'		=> array(
 		1	=> '1 час',
 		2	=> '%d часа',
 		3	=> '%d часов',
 	),
-	'MINUTE'	=> array(
+	'R_MINUTE'	=> array(
 		1	=> '1 минута',
 		2	=> '%d минуты',
 		3	=> '%d минут',
 	),
-	'SECOND'	=> array(
+	'R_SECOND'	=> array(
 		1	=> '1 секунда',
 		2	=> '%d секунды',
 		3	=> '%d секунд',
 	),
 
-	'JANUARY'	=> 'Январь',
-	'FEBRUARY'	=> 'Февраль',
-	'MARCH'		=> 'Март',
-	'APRIL'		=> 'Апрель',
-	'MAY'		=> 'Май',
-	'JUNE'		=> 'Июнь',
-	'JULY'		=> 'Июль',
-	'AUGUST'	=> 'Август',
-	'SEPTEMBER'	=> 'Сентябрь',
-	'OCTOBER'	=> 'Окстябрь',
-	'NOVEMBER'	=> 'Ноябрь',
-	'DECEMBER'	=> 'Декабрь',
+	'R_JANUARY'		=> 'Январь',
+	'R_FEBRUARY'	=> 'Февраль',
+	'R_MARCH'		=> 'Март',
+	'R_APRIL'		=> 'Апрель',
+	'R_MAY'			=> 'Май',
+	'R_JUNE'		=> 'Июнь',
+	'R_JULY'		=> 'Июль',
+	'R_AUGUST'		=> 'Август',
+	'R_SEPTEMBER'	=> 'Сентябрь',
+	'R_OCTOBER'		=> 'Октябрь',
+	'R_NOVEMBER'	=> 'Ноябрь',
+	'R_DECEMBER'	=> 'Декабрь',
 
-	'MONDAY'	=> 'Понедельник',
-	'TUESDAY'	=> 'Вторник',
-	'WEDNESDAY'	=> 'Среда',
-	'THURSDAY'	=> 'Четверг',
-	'FRIDAY'	=> 'Пятница',
-	'SATURDAY'	=> 'Суббота',
-	'SUNDAY'	=> 'Воскресенье',
+	'R_MONDAY'		=> 'Понедельник',
+	'R_TUESDAY'		=> 'Вторник',
+	'R_WEDNESDAY'	=> 'Среда',
+	'R_THURSDAY'	=> 'Четверг',
+	'R_FRIDAY'		=> 'Пятница',
+	'R_SATURDAY'	=> 'Суббота',
+	'R_SUNDAY'		=> 'Воскресенье',
 ));
